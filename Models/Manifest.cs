9f3914eb24fe21f9f076255d3f96393fc25a59ad
Arkadii <50463369+kawasaniac@users.noreply.github.com>
2025-08-03 13:00:02 +0200

    Added useful data annotations for manifest model

--- Models/Manifest.cs
+++ Models/Manifest.cs
@@ -1,16 +1,26 @@
-﻿namespace MaelstromLauncher.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaelstromLauncher.Server.Models
 {
     public class Manifest
     {
+        [Required]
         public string Version { get; set; } = "1.0";
+        [Required]
         public DateTime GeneratedAt { get; set; } = DateTime.UtcNow;
+        [Required]
         public List<FileEntry> Files { get; set; } = [];
     }
     public class FileEntry
     {
+        [Required]
         public string Path { get; set; } = "";
+        [Range (0, long.MaxValue)]
         public long Size { get; set; }
+        [Required]
         public string Hash { get; set; } = "";
+        [Required]
+        [Url]
         public string Url { get; set; } = "";
     }
 }
